9ef5bfbbe4567dcc0e5e1d3b98c911ec15c5a637
victor <victor.arbiol@gmail.com>
2025-04-12 22:24:08 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './', // Cambia la base URL a relativa
   server: {
     host: "::",
     port: 8080,
